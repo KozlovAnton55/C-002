03a173e8ba982bd383f3c77a29ffaa9ec246aaad
Anton <Realkom55a@Yandex.ru>
2022-10-23 23:17:20 +0300

    Коммит 004

--- array/Program.cs
+++ array/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int Max(int arg1, int arg2, int arg3)
+        {
+            int result = arg1;
+            if (arg2 > result) result = arg2; if (arg3 > result) result = arg3;
+            return result;
+        }
+
+        int[] array = { 32, 45, 56, 89, 122, 345, 111100, 543, 456 };
+
+        int result = Max(Max(array[0], array[1], array[2])
+        , Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+        Console.WriteLine(result);
+    }
+}
