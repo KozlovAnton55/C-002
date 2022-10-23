ca48508511dd4f1916082b84b4e2e21ba66171b3
Anton <Realkom55a@Yandex.ru>
2022-10-23 22:30:12 +0300

    Коммит 003

--- project001/Program.cs
+++ project001/Program.cs
@@ -1,16 +1,26 @@
-﻿int a1 = 12;
-int b1 = 48;
-int c1 = 65;
-int a2 = 87;
-int b2 = 95;
-int c2 = 100;
-int a3 = 31;
-int b3 = 45;
-int c3 = 54;
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int Max(int arg1, int arg2, int arg3)
+        {
+            int result = arg1;
+            if (arg2 > result) result = arg2; if (arg3 > result) result = arg3;
+            return result;
+        }
 
-int max = a1;
+        int a1 = 12;
+        int b1 = 48;
+        int c1 = 65;
+        int a2 = 87;
+        int b2 = 95;
+        int c2 = 100;
+        int a3 = 31;
+        int b3 = 45;
+        int c3 = 54;
 
-if (b1 > max) max = b1; if (c1 > max) max = c1; if (a2 > max) max = a2;
-if (b2 > max) max = b2; if (c2 > max) max = c2; if (a3 > max) max = a3;
-if (b3 > max) max = b3; if ( c3 > max) max = c3;
-Console.WriteLine(max);
+        int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+        Console.WriteLine(max);
+    }
+}
