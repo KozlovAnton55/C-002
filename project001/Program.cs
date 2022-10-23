5946faffc5eb9a6a9e905c18453b34e4b391b275
Anton <Realkom55a@Yandex.ru>
2022-10-23 22:06:56 +0300

    Коммит 002

--- project001/Program.cs
+++ project001/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿int a1 = 12;
+int b1 = 48;
+int c1 = 65;
+int a2 = 87;
+int b2 = 95;
+int c2 = 100;
+int a3 = 31;
+int b3 = 45;
+int c3 = 54;
+
+int max = a1;
+
+if (b1 > max) max = b1; if (c1 > max) max = c1; if (a2 > max) max = a2;
+if (b2 > max) max = b2; if (c2 > max) max = c2; if (a3 > max) max = a3;
+if (b3 > max) max = b3; if ( c3 > max) max = c3;
+Console.WriteLine(max);
