e9fef957f8b9d003742902881eec88a34cee4d90
Anton <Realkom55a@Yandex.ru>
2022-10-25 23:57:07 +0300

    Коммит 006

--- index/Program.cs
+++ index/Program.cs
@@ -2,7 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        int[] array = { 123, 234, 356, 47, 58, 69, 74, 86 };
+        int[] array = { 123, 234, 356, 47, 58, 69,234, 74, 86 };
         int n = array.Length;
         int find = 47;
         int index = 0;
@@ -12,6 +12,7 @@
             if (array[index] == find)
             {
                 Console.WriteLine(index);
+                break;
             }
 
             index++;
