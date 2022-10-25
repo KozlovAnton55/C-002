c97f11432fed70a93386685bd7dbe45f899f2f6e
Anton <Realkom55a@Yandex.ru>
2022-10-25 23:52:29 +0300

    Коммит 006

--- index/Program.cs
+++ index/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = { 123, 234, 356, 47, 58, 69, 74, 86 };
+        int n = array.Length;
+        int find = 47;
+        int index = 0;
+
+        while (index < n)
+        {
+            if (array[index] == find)
+            {
+                Console.WriteLine(index);
+            }
+
+            index++;
+
+        }
+    }
+}
