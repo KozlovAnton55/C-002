6a0853c2f0af477048fd7bdd9e708028a2540e50
Anton <Realkom55a@Yandex.ru>
2022-10-26 21:08:06 +0300

    Коммит 008

--- indexRan/Program.cs
+++ indexRan/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        void FillArray(int[] collection)
+        {
+            int lenght = collection.Length;
+            int index = 0;
+            while (index < lenght)
+            {
+                collection[index] = new Random().Next(1, 10);
+                index++;
+            }
+        }
+
+
+        void PrintArray(int[] col)
+        {
+            int count = col.Length;
+            int position = 0;
+            while (position < count)
+            {
+                Console.WriteLine(col[position]);
+                position++;
+            }
+        }
+
+        int[] array = new int[10];
+
+        FillArray(array);
+        PrintArray(array);
+    }
+}
